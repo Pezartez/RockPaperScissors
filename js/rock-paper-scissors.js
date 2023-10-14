c7121b88d176437c743650de39cb073c4844a77d
Pezartez <kathryn.l.pereira@gmail.com>
2023-10-13 21:13:25 -0400

    Add notes to js file to form a programming plan

---
+++ js/rock-paper-scissors.js
@@ -0,0 +1,22 @@
+/* 
+1. first function should output computer's choice
+rock,paper or scissors (random select)
+
+*/
+function getComputerChoice() {
+
+}
+
+/* 2. second function will play through
+one round of rps using two parameters:
+playerSelection -> should be case insensitive
+computerSelection
+
+example of return string: "You Lose! Paper beats Rock"
+
+*/
+function playRound(playerSelection, computerSelection){
+
+}
+/* wrap playRound in a different function that plays through
+ 5 rounds before reporting winner or loser*/
