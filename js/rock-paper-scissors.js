725ab3677140a83436d7fdbc57244f0b12e5eecf
Pezartez <kathryn.l.pereira@gmail.com>
2023-10-14 22:31:59 -0400

    Write function playGame to call five rounds and report winner

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,20 +1,16 @@
-// Selects Computer Choice
-let choices = ["Rock", "Paper", "Scissors"];
+
+    let choices = ["Rock", "Paper", "Scissors"];
+    let playerScore = 0;
+
+// selects computer choice
 function getComputerChoice() {
-    let choice = choices[(Math.floor(Math.random() * choices.length))];
-    return choice;
-}
+        let choice = choices[(Math.floor(Math.random() * choices.length))];
+        return choice;}
 
-/* 2. second function will play through
-one round of rps using two parameters:
-playerSelection -> should be case insensitive
-computerSelection
-
-example of return string: "You Lose! Paper beats Rock"
-
-*/
-function playRound(){
+    //plays one round of the game
+function playRound() {
     // Uses player input to choose rock, paper, or scissors
+
     let playerTypedChoice = prompt("Do you choose 'Rock', 'Paper', or 'Scissors'?");
     if (playerTypedChoice.toLowerCase() == "rock") {
         playerSelection = choices[0];
@@ -43,19 +39,30 @@ function playRound(){
         console.log("Your Lose! Paper beats Rock");
     } else if (playerSelection === choices[0] && computerSelection === choices[2]) {
         console.log("You Win! Rock beats Scissors");
+        playerScore++;
     } else if (playerSelection === choices[1] && computerSelection === choices[0]) {
         console.log("You Lose! Rock beats Paper");
     } else if (playerSelection === choices[1] && computerSelection === choices[2]) {
         console.log("You Win! Rock beats Scissors");
+        playerScore++;
     } else if (playerSelection === choices[2] && computerSelection === choices[0]) {
         console.log("You Lose! Rock beats Scissors");
     } else if (playerSelection === choices[2] && computerSelection === choices[1]) {
         console.log("You Win! Scissors beat Paper");
-    }//nead to add points feature for when we play 5 rounds.
+        playerScore++;
+    }
+    console.log(playerScore);
 }
 
+function playGame() {
+    for (let round = 1; round < 6; round++) {
+        playRound();
+    }
 
+    if (playerScore >= 3) {
+        console.log("You are the winner, congratulations!");
+    } else {
+        console.log("You did not beat the computer, try again next time!");
+    }
 
-
-/* wrap playRound in a different function that plays through
- 5 rounds before reporting winner or loser*/
+}
